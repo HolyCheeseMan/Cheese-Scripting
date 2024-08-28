3aa781b311370ce65cce0d1e7c3c2b4f3c424b04
Holy Cheese Man <lightwhoosh@gmail.com>
2024-08-28 16:52:45 +1200

    0.1.2 Installer

--- 0.1.2-Installer.bat
+++ 0.1.2-Installer.bat
@@ -6,10 +6,18 @@ title Cheese Scripting Installer 0.1.2
 cls
 @echo off
 cls
-powershell write-host -back darkred -fore white This is an OPTIONAL Cheese Scripting Installer.
+powershell write-host -back darkred -fore white This is an OPTIONAL But RECOMMENDED Cheese Scripting Installer.
 powershell write-host -back red -fore white You may run Cheese Scripting alone 'if' wanted.
 powershell write-host -fore white  
-powershell write-host -back green -fore white Write '"1"' To Install.
+powershell write-host -back darkyellow -fore white Please give this installer file Administrator permission 'if' you see this:
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+echo ERROR: Access is denied.
+powershell write-host -back darkgreen -fore white Write '"1"' To Install.
 set /p installop= 
 if "%installop%"=="1" goto installprocess
 goto startscriptinstall
@@ -66,8 +74,17 @@ cscript //nologo "%temp%\CreateShortcut.vbs"
 
 del "%temp%\CreateShortcut.vbs"
 
+@echo del "C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptig\APP\Cheese Scripting.bat">"C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptig\APP\Unistall.bat"
+
 echo Asset Installed
 echo Configuring...
+set "regKey=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\CheeseScripting"
+set "appName=Cheese Scripting"
+set "appVersion=0.1.0"
+set "appPublisher=Holy Cheese Man"
+set "appInstallLocation=C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScripting\APP\Cheese Scripting.bat"
+set "appUninstallString=C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptig\APP\Unistall.bat"
+echo Checking...
 @echo off
 set "targetBatchPath=C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScripting\APP\Cheese Scripting.bat"
 set "shortcutPath=C:\Users\%USERNAME%\downloads\Cheese Scripting.lnk"
@@ -80,6 +97,15 @@ echo oLink.IconLocation = "%iconPath%, 0" >> "%temp%\CreateShortcut.vbs"
 echo oLink.Save >> "%temp%\CreateShortcut.vbs"
 
 cscript //nologo "%temp%\CreateShortcut.vbs"
+echo Setting Up...
+
+reg add "%regKey%" /v "DisplayName" /t REG_SZ /d "%appName%" /f
+reg add "%regKey%" /v "DisplayVersion" /t REG_SZ /d "%appVersion%" /f
+reg add "%regKey%" /v "Publisher" /t REG_SZ /d "%appPublisher%" /f
+reg add "%regKey%" /v "InstallLocation" /t REG_SZ /d "%appInstallLocation%" /f
+reg add "%regKey%" /v "UninstallString" /t REG_SZ /d "%appUninstallString%" /f
+reg add "%regKey%" /v "QuietUninstallString" /t REG_SZ /d "%appUninstallString%" /f
+reg add "%regKey%" /v "InstallDate" /t REG_SZ /d "%date:~10,4%%date:~4,2%%date:~7,2%" /f
 
 del "%temp%\CreateShortcut.vbs"
 echo Opening Downloads Folder...
